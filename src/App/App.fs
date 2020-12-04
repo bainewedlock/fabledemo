9f9b09200f9044e8a6b636ee5784a9450918dabf
boeckwi <w.boeck@walz.de>
2020-12-04 13:22:55 +0100

    button

--- src/App/App.fs
+++ src/App/App.fs
@@ -1,9 +1,11 @@
 ﻿module App
 
+open Fable.Core
 open Browser.Types
 
 let window = Browser.Dom.window
-let canvas = window.document.getElementById "myCanvas" :?> HTMLCanvasElement
+let getById (id:string) = window.document.getElementById id
+let canvas = getById "canvas" :?> HTMLCanvasElement
 let ctx = canvas.getContext_2d()
 
 canvas.width <- 400.
@@ -15,6 +17,9 @@ ctx.stroke()
 
 ctx.textAlign <- "center"
 ctx.fillText("Hello world!", 200., 200.)
+
+let button1 = getById "button1" :?> HTMLButtonElement
+button1.onclick <- fun e ->  printfn "button clicky: %A" e
 
 printfn "done!"
 
