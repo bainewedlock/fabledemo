ce02aca536bcc47800414611b5659f3bea4489b8
boeckwi <w.boeck@walz.de>
2020-12-05 08:46:09 +0100

    draw

--- src/App/App.fs
+++ src/App/App.fs
@@ -7,20 +7,22 @@ let window = Browser.Dom.window
 let getById (id:string) = window.document.getElementById id
 let canvas = getById "canvas" :?> HTMLCanvasElement
 let ctx = canvas.getContext_2d()
-
 canvas.width <- 400.
 canvas.height <- 400.
-
-ctx.moveTo(0., 0.)
-ctx.lineTo(100., 100.)
-ctx.stroke()
-
-ctx.textAlign <- "center"
-ctx.fillText("Hello world!", 200., 200.)
 
 let button1 = getById "button1" :?> HTMLButtonElement
 button1.onclick <- fun e ->  printfn "button clicky: %A!" e
 
-printfn "done!"
+ctx.scale(10., 10.)
+ctx.translate(-490., 10.)
 
+let drawPixel (x, y) = ctx.rect(float x, float y, 0.5, 0.5)
 
+let drawPixels (color:string) ps =
+    ctx.strokeStyle <- U3.Case1 color
+    ps |> Seq.iter drawPixel
+
+let g = Solution.demogrid
+drawPixels "#000000" g.sand
+
+ctx.stroke()
